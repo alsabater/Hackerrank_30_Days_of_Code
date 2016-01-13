252d81d86382cde3e6ec428798cb562e55eb4773
alsabater <alvaro@sabater.eu>
2016-01-13 18:17:00 +0100

    Convert_to_binary

---
+++ Convert_to_binary.rb
@@ -0,0 +1,29 @@
+t = gets.chomp.to_i
+def convertBinary(n)
+	num = 0
+	bin = ""
+	e = n
+	i = 0
+	while num!=n do
+		if e%2===0
+			bin = bin + "0"
+			x = 0
+			num += x*(2**(i))
+			i+=1
+			e=e/2
+		elsif e%2===1
+			bin = bin + "1"
+			x = 1
+			num +=x*(2**(i))
+			i+=1
+			e=e/2
+		end
+	end
+	puts bin.reverse
+end
+
+while t>0 do
+	n = gets.chomp.to_i
+	convertBinary(n)
+	t-=1
+end
